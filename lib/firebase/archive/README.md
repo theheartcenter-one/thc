235be1ff8d780d0826083e176d99b45a6230a0db
Nate Wilson <nate.w5687@gmail.com>
2024-05-06 22:15:18 -0600

    organize files

---
+++ lib/firebase/archive/README.md
@@ -0,0 +1,3 @@
+The `firebase/archive/` directory has class declarations that integrate with our backend but haven't been incorporated into the app yet.
+
+(When you decide to use one of these, you should move it to a different folder 🙂)
--- lib/firebase/firebase.dart
+++ lib/firebase/firebase.dart
@@ -1,7 +1,7 @@
 import 'package:thc/firebase/src/user.dart';
 
-export 'src/fetch_survey.dart';
-export 'src/livestream.dart';
+export 'archive/fetch_survey.dart';
+export 'archive/livestream.dart';
 export 'src/user.dart';
 export 'src/user_type.dart';
 export 'src/firestore.dart';
